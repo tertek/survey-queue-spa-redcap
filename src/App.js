08c0547b0036abe1bfb0408ad204d3d8c990bdfd
tertek <ekin.tertemiz@swisstph.ch>
2021-06-09 11:44:12 +0200

    Add main layout.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,21 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 import './App.css';
 
-const apiUrl = 'http://redcap.test/plugins/dashboard';
+import Dashboard from './components/Dashboard';
+import LoginScreen from './components/LoginScreen';
 
-axios.interceptors.request.use(
-  config => {
-    const { origin } = new URL(config.url);
-    const allowedOrigins = [apiUrl];
-    const token = localStorage.getItem('token');    
-    if (allowedOrigins.includes(origin)) {
-      config.headers.authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
 
 function App() {
 
-  const storedJwt = localStorage.getItem('token');
-  const [test, setTest] = useState([]);
-  const [jwt, setJwt] = useState(storedJwt || null);
-  const [fetchError, setFetchError] = useState(null);
-
-  const getJwt = async () => {
-    const { data } = await axios.get(`${apiUrl}/dashboard_endpoint.php`);
-    localStorage.setItem('token', data.token);
-    setJwt(data.token);
-  };
-
-  const getTest = async () => {
-    try {
-      const { data } = await axios.get(`${apiUrl}/test.php`)
-      setTest(data);
-      setFetchError(null);
-    } catch (err) {
-      setFetchError(err.message);
-    }
-  };
+  //  App States
+  //  (https://serverless-stack.com/chapters/load-the-state-from-the-session.html)
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
 
   return (
-      <>
-      <section style={{ marginBottom: '10px' }}>
-        <button onClick={() => getJwt()}>Get JWT</button>
-        {jwt && (
-          <pre>
-            <code>{jwt}</code>
-          </pre>
-        )}
-      </section>
-      <section>
-        <button onClick={() => getTest()}>
-          Get Test
-        </button>
-        {test && (
-          <pre>
-            <code>{test}</code>
-          </pre>
-        )}
-        {fetchError && (
-          <p style={{ color: 'red' }}>{fetchError}</p>
-        )}
-      </section>
-    </>
-  );
+    isAuthenticated ? ( <Dashboard></Dashboard> ) : ( <LoginScreen></LoginScreen> )
+  )
+  
 }
 
 export default App;
