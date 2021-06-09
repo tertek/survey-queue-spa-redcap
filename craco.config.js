b28ed7fcd576d4d6a5fdd4bcb3d1ae6d1bff0265
tertek <ekin.tertemiz@swisstph.ch>
2021-06-09 11:12:31 +0200

    Add tailwind.css

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+// https://tailwindcss.com/docs/guides/create-react-app
+
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }
