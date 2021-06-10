fdd7a65a8dffba1f13dc811c1e3491d3c4fa68fa
tertek <ekin.tertemiz@swisstph.ch>
2021-06-10 14:36:27 +0200

    Add Login Form

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -3,7 +3,47 @@ import logo from './../stph.svg';
 
 class LoginScreen extends React.Component {
 
+
+    getOptionsForDay() {
+        var items = [];
+        for (let index = 1; index <= 31; index++) {
+            items.push(<option key={index}>{index}</option>);
+        }
+        return items;
+    }
+
+    getMonthNames(locale = 'en', format = 'long') {        
+        const formatter = new Intl.DateTimeFormat(locale, { month: format, timeZone: 'UTC' });
+        const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(month => {
+            const mm = month < 10 ? `0${month}` : month;
+            return new Date(`2017-${mm}-01T00:00:00+00:00`);
+        });
+    return months.map(date => formatter.format(date));
+    }
+
+    getOptionsForMonth() {
+        var items = [];
+        var months = this.getMonthNames(navigator.language.slice(0,2));
+        months.forEach((element, index) => {
+            items.push(<option key={index}>{element}</option>);
+        })
+        return items;
+    }
+
+    getOptionsForYear() {
+        var items = [];
+        for (let index = 1920; index <= 2020; index++) {
+            items.push(<option key={index}>{index}</option>);
+        }
+        return items;
+    }
+
+
     render() {
+
+        const optionsForDay = this.getOptionsForDay();
+        const optionsForMonth = this.getOptionsForMonth();
+        const optionsForYear = this.getOptionsForYear();
 
         return(
             <div class="w-full flex flex-wrap">                
@@ -14,16 +54,32 @@ class LoginScreen extends React.Component {
                     </div>
 
                     <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-                        <p class="text-center text-3xl">Welcome.</p>
+                        <p class="text-center text-3xl">Survey Dashboard</p>
                         <form class="flex flex-col pt-3 md:pt-8" onsubmit="event.preventDefault();">
                             <div class="flex flex-col pt-4">
-                                <label for="email" class="text-lg">Email</label>
-                                <input type="email" id="email" placeholder="your@email.com" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
+                                <label for="oid" class="text-lg">Participant ID</label>
+                                <input type="text" id="oid" placeholder="086564211" class="shadow appearance-none border-none rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:ring-indigo-200 focus:ring-opacity-50"></input>
                             </div>
             
                             <div class="flex flex-col pt-4">
-                                <label for="password" class="text-lg">Password</label>
-                                <input type="password" id="password" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
+                                <label for="email" class="text-lg">Date of Birth</label>
+                                <div class="inline-flex">
+
+                                <select class="rounded mr-2 mt-1 border-none shadow focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700 leading-tight">
+                                    <option value="">Day</option>
+                                    {optionsForDay}
+                                </select>   
+
+                                <select class="flex-grow rounded mr-2 mt-1 border-none shadow focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700 leading-tight">
+                                    <option value="">Month</option>
+                                    {optionsForMonth}
+                                </select>
+      
+                                <select class="rounded mt-1 border-none shadow focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700 leading-tight">
+                                    <option value="">Year</option>
+                                    {optionsForYear}
+                                </select>                                
+                                </div>
                             </div>
             
                             <input type="submit" value="Log In" class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8"></input>
@@ -36,7 +92,7 @@ class LoginScreen extends React.Component {
 
                 </div>
                 <div class="w-3/5">
-                    <img class="object-cover w-full h-screen hidden md:block" src={process.env.PUBLIC_URL + '/bg.png'} />
+                    <img class="object-cover w-full h-screen hidden md:block" src={process.env.PUBLIC_URL + '/bg.jpg'} />
                 </div>
             </div>
         );
