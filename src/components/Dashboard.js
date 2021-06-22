9341cf6288c4c884d61364fe7c326f319671e271
tertek <ekin.tertemiz@swisstph.ch>
2021-06-22 10:26:08 +0200

    Test Pusher

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,15 +3,31 @@ import Loading from './Loading';
 import Navbar from './Navbar';
 import Header from './Header';
 import Main from './Main';
+import Pusher from 'pusher-js';
 
 
 class Dashboard extends React.Component {
 
-    constructor(props) {
+/*     constructor(props) {
         super(props);
-    }
+    } */
     
     async componentDidMount() {
+
+
+            // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('5cd48aa364da3059e49f', {
+        cluster: 'eu'
+        });
+
+        var channel = pusher.subscribe('my-channel');
+        channel.bind('my-event', function(data) {
+        alert(JSON.stringify(data));
+        });
+
+
         setTimeout(() => {
             this.props.onHasFinishedLoading();
         },2000);
